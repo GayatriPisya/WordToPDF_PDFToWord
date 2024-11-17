1d7998ba15220565a24ac9427e13ae6cdb20b1b1
Gayatri Pisya <gayatripisya@gmail.com>
2024-11-17 11:49:58 +0530

    updated

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ def upload_file():
             return f"Conversion failed: {e}", 500
     else:
         return "Invalid file type", 400
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
